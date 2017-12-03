3ec9b332c408f39f4542ab41793bb99e619df04c
Shahzad Emambaccus <shay_619@hotmail.co.uk>
2017-12-03 23:45:01 +0000

    GUI Implementation
    I have implemented the graphic user interface into the application, including all buttons in the correct location. Next step is to implement the text to symbols and signs using dumy information

--- FinalYearProjectUser/app/src/main/java/com/example/shahzademambaccus/finalyearprojectuser/SignsAndSymbols.java
+++ FinalYearProjectUser/app/src/main/java/com/example/shahzademambaccus/finalyearprojectuser/SignsAndSymbols.java
@@ -2,17 +2,12 @@ package com.example.shahzademambaccus.finalyearprojectuser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class Categories extends AppCompatActivity {
+public class SignsAndSymbols extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_categories);
-    }
-
-    public void symbolPressed(View v){
-
+        setContentView(R.layout.activity_signs_and_symbols);
     }
 }
